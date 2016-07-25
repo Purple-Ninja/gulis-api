ccdcef63538120b864e41bad70288e3767011038
Maxis Kao <maxiskao@yahoo-inc.com>
2016-07-25 16:53:41 +0800

    add simple search api

--- app.js
+++ app.js
@@ -3,13 +3,42 @@
 
 var express = require('express');
 var bodyParser = require('body-parser');
-
+var MongoClient = require('mongodb').MongoClient;
+var mongouri = process.env.MONGOURI || 'mongodb://localhost:27017/slack';
 var app = express();
+var db;
 
 app.set('port', process.env.PORT || 5000);
 
 app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+MongoClient.connect(mongouri, function(err, database) {
+    console.log('[mongo] connected to %s', mongouri);
+
+    // global variable for the mongo db
+    db = database;
+
+    // start server after connecting to mongodb
+    app.listen(app.get('port'), function() {
+        console.log('[node] app is running on port', app.get('port'));
+    });
+});
+
+
+var getOneImage = function (postid, callback) {
+    var query = {
+        'post_id': postid
+    };
+    db.collection('beauty.posts')
+        .find(query)
+        .sort({ display: 1 })
+        .limit(1)
+        .toArray(function(err, docs) {
+            callback(err, docs[0] || {});
+        }
+    );
+};
 
 /*
  * entry for search
@@ -20,18 +49,25 @@ app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x
  *  limit: 2
  */
 app.get('/beauty/search', function(req, res) {
-    var data = {
-        suggestions: [{
-            title: 'Hi',
-            push: 100,
-            url: 'http://i.imgur.com/IDfkVvc.png'
-        },{
-            title: 'Hihi',
-            push: 50,
-            url: 'http://i.imgur.com/EIH9cNu.jpg'
-        }]
+    var query = {
+        tag: req.query.tag || '正妹',
+        fetched: true,
+        push: { $gte: Number(req.query.push) || 1 },
+        image_count: { $gte: 1 },
     };
-    res.status(200).json(data);
+    if (req.query.keyword) {
+        query.title = new RegExp(req.query.keyword);
+    }
+    db.collection('beauty.lists')
+        .find(query)
+        .sort({ display: 1 })
+        .limit(1)
+        .toArray(function(err, docs) {
+            getOneImage(docs[0]['post_id'], function(err, img){
+                res.status(200).json(img);
+            });
+        }
+    );
 });
 
 /*
@@ -75,10 +111,6 @@ app.get('/beauty/trending', function(req, res) {
     res.status(200).json(data);
 });
 
-// Start server
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
-});
 
 module.exports = app;
 
